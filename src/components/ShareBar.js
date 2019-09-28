01e3cac5ad952b9004fa14df884318bdf32bd9b3
dandroos <dandrewsuk82@gmail.com>
2019-09-28 16:44:51 +0100

    fixed window error

--- src/components/ShareBar.js
+++ src/components/ShareBar.js
@@ -9,46 +9,66 @@ import {
   WhatsappIcon,
   EmailIcon,
 } from "react-share"
-import { FormattedMessage } from 'react-intl'
+import { FormattedMessage } from "react-intl"
 import { Button } from "reactstrap"
 
 export default function ShareBar(props) {
-  const url = window.location.href
-  const iconSize = 32
+  if (typeof window !== "undefined") {
+    const url = window.location.href
+    const iconSize = 32
 
-  return (
-    <div className="py-2 px-1 my-2 text-dark">
+    return (
+      <div className="py-2 px-1 my-2 text-dark">
         <span className="mr-3">
-            <FormattedMessage id="share" />:
+          <FormattedMessage id="share" />:
         </span>
-      <FacebookShareButton url={url} className="d-inline-block">
-        <Button color="primary" className="p-0 mr-1">
-          <FacebookIcon size={iconSize} iconBgStyle={{
-              fill: 'transparent'
-          }} className="share-btn" />
-        </Button>
-      </FacebookShareButton>
-      <TwitterShareButton url={url} className="d-inline-block">
-        <Button color="primary" className="p-0 mr-1">
-          <TwitterIcon size={iconSize} iconBgStyle={{
-              fill: 'transparent'
-          }} className="share-btn" />
-        </Button>
-      </TwitterShareButton>
-      <WhatsappShareButton url={url} className="d-inline-block">
-        <Button color="primary" className="p-0 mr-1">
-          <WhatsappIcon size={iconSize} iconBgStyle={{
-              fill: 'transparent'
-          }} className="share-btn" />
-        </Button>
-      </WhatsappShareButton>
-      <EmailShareButton url={url} className="d-inline-block">
-        <Button color="primary" className="p-0">
-          <EmailIcon size={iconSize} iconBgStyle={{
-              fill: 'transparent'
-          }} className="share-btn" />
-        </Button>
-      </EmailShareButton>
-    </div>
-  )
+        <FacebookShareButton url={url} className="d-inline-block">
+          <Button color="primary" className="p-0 mr-1">
+            <FacebookIcon
+              size={iconSize}
+              iconBgStyle={{
+                fill: "transparent",
+              }}
+              className="share-btn"
+            />
+          </Button>
+        </FacebookShareButton>
+        <TwitterShareButton url={url} className="d-inline-block">
+          <Button color="primary" className="p-0 mr-1">
+            <TwitterIcon
+              size={iconSize}
+              iconBgStyle={{
+                fill: "transparent",
+              }}
+              className="share-btn"
+            />
+          </Button>
+        </TwitterShareButton>
+        <WhatsappShareButton url={url} className="d-inline-block">
+          <Button color="primary" className="p-0 mr-1">
+            <WhatsappIcon
+              size={iconSize}
+              iconBgStyle={{
+                fill: "transparent",
+              }}
+              className="share-btn"
+            />
+          </Button>
+        </WhatsappShareButton>
+        <EmailShareButton url={url} className="d-inline-block">
+          <Button color="primary" className="p-0">
+            <EmailIcon
+              size={iconSize}
+              iconBgStyle={{
+                fill: "transparent",
+              }}
+              className="share-btn"
+            />
+          </Button>
+        </EmailShareButton>
+      </div>
+    )
+  } else {
+    return null
+  }
 }
